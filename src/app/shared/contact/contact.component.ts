1e5e776cfd9b40196f08e14f5a5c1dfdbb60239c
Rawin Kumar <robinmedco2002@gmail.com>
2020-07-27 21:14:44 +0530

    saving to Github

--- src/app/shared/contact/contact.component.ts
+++ src/app/shared/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-contact',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ContactComponent implements OnInit {
 
   topics =['Angular', 'React', 'Vue'];
+  userModel = new User('', '', 1000000000, '', '', '');
 
   constructor() { }
 
---
+++ src/app/shared/contact/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    constructor(
+        public name: string,
+        public email: string,
+        public phone: number,
+        public topic: string,
+        public subject: string,
+        public message: string,
+    ) {}
+}
